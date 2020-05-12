5bf9c871aa286bbf39dc2563a91f9a7689b7c7c7
Reza <64578268+Unmd0@users.noreply.github.com>
2020-05-12 19:35:10 +0430

    update xmenu function
    Now we run just 1 time Query builder

--- xmenu.blade.php
+++ xmenu.blade.php
@@ -1,24 +1,41 @@
 @php
+
+
+
+
 function xmenu($X,$X_table,$X_cloumn,$X_id)
     {
-        //GET All Category From Your Database
-        $items = DB::table($X_table)->where($X_id,"=",$X)->get();
+        $items = DB::table($X_table)->get()->toArray();
+        X_menu_stack($X,$X_cloumn,$X_id,$items);
+    }
+
+
+
+
+
+function X_menu_stack($X,$X_cloumn,$X_id,$items)
+    {
+        $menu = array_filter($items, function($val) use($X,$X_id){
+            return $val->$X_id == $X;
+        });
             //Check for empty STACK
-            if (count($items) > 0)
+            if (count($menu) > 0)
                 {
-                    //if you have class for Child ul add here
+                    //if you have class for Parent ul add here
                     $class="header__menu__dropdown";
                     if ($X==0){$class="";}
                     echo('<ul class="'.$class.'">');
-                    foreach ($items as $item) 
+                    foreach ($menu as $item) 
                         {
                         echo('<li><a href="$">');
                         echo( $item->$X_cloumn );
                         echo('</a>');
-                        xmenu($item->id,$X_table,$X_cloumn,$X_id);
+                        X_menu_stack($item->id,$X_cloumn,$X_id,$items);
                         echo('</li>');
                         }
                     echo('</ul>');
-                }
+                }        
     }
+
+
 @endphp
